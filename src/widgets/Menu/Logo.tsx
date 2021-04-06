c0a94615ed890bfa8d29b082bb747ab63d386e60
Vishal <vishalkumar750372@gmail.com>
2021-04-06 10:38:02 +0530

    HomePage Logo Width Increased

--- src/widgets/Menu/Logo.tsx
+++ src/widgets/Menu/Logo.tsx
@@ -23,7 +23,7 @@ const StyledLink = styled(Link)`
     }
   }
   .desktop-icon {
-    width: 200px;
+    width: 240px;
     display: none;
     ${({ theme }) => theme.mediaQueries.nav} {
       display: block;
