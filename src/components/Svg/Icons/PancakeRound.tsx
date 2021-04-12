a4e028d9beb2810ad5b0a814b036230c455a0587
Vishal <vishalkumar750372@gmail.com>
2021-04-12 19:28:26 +0530

    Logo with circle image changed

--- src/components/Svg/Icons/PancakeRound.tsx
+++ src/components/Svg/Icons/PancakeRound.tsx
@@ -5,7 +5,7 @@ import { SvgProps } from "../types";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <div style={{'marginRight': '15px'}}>
-      <img alt="logo" style={{width: "20px"}} src="/images/egg/logo.png" />
+      <img alt="logo" style={{width: "35px"}} src="/images/logo-with-circle.png" />
     </div>
   );
 };
